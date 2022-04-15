d8bb75e8b973ce18f9060f40c426162c08145a09
Coach Bridget <cbridget@su-b4.msk.21-school.ru>
2022-04-15 16:51:42 +0300

    need a dead

--- philo/hdrs/philosophers.h
+++ philo/hdrs/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: cbridget <cbridget@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 17:47:24 by cbridget          #+#    #+#             */
-/*   Updated: 2022/04/13 19:13:29 by cbridget         ###   ########.fr       */
+/*   Updated: 2022/04/14 17:15:56 by cbridget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 # include <stdlib.h>
 # include <pthread.h>
 # include <unistd.h>
+# include <sys/time.h>
+
+typedef unsigned long long big_num;
 
 typedef struct s_args {
 	int	num_phils;
@@ -24,10 +27,20 @@ typedef struct s_args {
 	int	time_eat;
 	int	time_sleep;
 	int	num_time_eat;
+	big_num	start_t;
+	struct timeval	tv;
 } t_args;
 
+/*typedef struct s_forks {
+	pthread_mutex_t	mut_frk;
+	int				num_frk;
+} t_forks;*/
+
 typedef struct s_th_phil {
-	pthread_t	th_p;
+	pthread_t		th_phil;
+	pthread_mutex_t	mut_frk;
+	int				number;
+	t_args			*args;
 } t_th_phil;
 
 int	parser(int argc, char **argv, t_args *args);
@@ -37,5 +50,10 @@ int	verify_argv(int argc, char **argv);
 int	ft_isdigit(int c);
 int	ft_atoi(const char *str, int *overflow);
 int	skipp(const char *str, int *sign, int *overflow);
+
+void	*th_phil(void *phil);
+void	sleeping_phil(t_th_phil	*phil);
+void	try_take_fork(pthread_mutex_t *mut_frk, int num, t_args *args);
+unsigned long long	get_time(t_args *args);
 
 # endif
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: cbridget <cbridget@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 17:56:07 by cbridget          #+#    #+#             */
-/*   Updated: 2022/04/13 19:14:28 by cbridget         ###   ########.fr       */
+/*   Updated: 2022/04/15 15:23:27by cbridget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 	pthread_t	test1[5];
 	pthread_mutex_t mutex1;
 	int x;
-} t_thread;*/
+} t_thread;
 
 void*	testing()
 {
-/*	printf("str= %s\n", (char *)str);
+	printf("str= %s\n", (char *)str);
 	printf("procces is %d\n", getpid());
 	usleep(500000);
 	printf("after sleep\n");
@@ -31,10 +31,10 @@ void*	testing()
 		((t_thread*)envm)->x++;
 		pthread_mutex_unlock(&((t_thread*)envm)->mutex1);
 	}
-	return NULL;*/
+	return NULL;
 	printf("thread has created\n");
 	return NULL;
-}
+}*/
 
 int	main(int argc, char **argv)
 {
@@ -61,24 +61,97 @@ int	main(int argc, char **argv)
 	t_th_phil	*phils;
 	int			i;
 
-	i = 0;
+	i = -1;
 	if (parser(argc, argv, &args))
 		return (3);
 	phils = (t_th_phil *)malloc(sizeof(t_th_phil) * args.num_phils);
 	if (!phils)
 		return (1);
+	gettimeofday(&args.tv, NULL);
+	args.start_t = (args.tv.tv_sec * 1000) + (args.tv.tv_usec / 1000);
+	while (++i < args.num_phils)
+		pthread_mutex_init(&phils[i].mut_frk, NULL);
+	i = 0;
 	while (i < args.num_phils)
 	{
-		if (pthread_create(&phils[i].th_p, NULL, testing, NULL))
+		phils[i].args = &args;
+		phils[i].number = i + 1;
+		printf("init mutes: %ld, num: %d\n", phils[i].mut_frk.__sig, phils[i].number);
+//		pthread_mutex_init(&phils[i].mut_frk, NULL);
+		if (pthread_create(&phils[i].th_phil, NULL, th_phil, &phils[i]))
 			return (1);
-		i++;
+//		pthread_mutex_init(&phils[i].mut_frk, NULL);
+		i++
 	}
 	i = 0;
 	while (i < args.num_phils)
 	{
-		if (pthread_join(phils[i].th_p, NULL))
+		if (pthread_join(phils[i].th_phil, NULL))
 			return (2);
+		pthread_mutex_destroy(&phils[i].mut_frk);
 		i++;
 	}
 	return (0);
+}
+
+void	*th_phil(void *phil)
+{
+	t_th_phil	*tmp_phil;
+
+	tmp_phil = phil;
+	while (1)
+	{
+		if (tmp_phil->number == 1)
+		{
+			try_take_fork(&tmp_phil->mut_frk, tmp_phil->number, tmp_phil->args);
+//			printf("lock num: %d\n", tmp_phil->number);
+			try_take_fork(&tmp_phil[tmp_phil->args->num_phils - 1].mut_frk, tmp_phil->number, tmp_phil->args);
+//			printf("lock num: %d\n", tmp_phil[tmp_phil->args->num_phils - 1].number);
+		}
+		else
+		{
+			try_take_fork(&(tmp_phil - 1)->mut_frk, tmp_phil->number, tmp_phil->args);
+//			printf("lock num: %d\n", (tmp_phil - 1)->number);
+			try_take_fork(&tmp_phil->mut_frk, tmp_phil->number, tmp_phil->args);
+//			printf("lock num: %d\n", tmp_phil->number);
+		}
+		printf("%llu %d is eating\n", get_time(tmp_phil->args), tmp_phil->number);
+		usleep(tmp_phil->args->time_eat * 1000);
+		sleeping_phil(tmp_phil);
+		printf("%llu %d is thinking\n", get_time(tmp_phil->args), tmp_phil->number);
+	}
+}
+
+void	sleeping_phil(t_th_phil	*phil)
+{
+	if (phil->number == 1)
+	{
+		pthread_mutex_unlock(&phil[phil->args->num_phils - 1].mut_frk);
+//		printf("unlock num: %d\n", phil[phil->args->num_phils - 1].number);
+		pthread_mutex_unlock(&phil->mut_frk);
+//		printf("unlock num: %d\n", phil->number);
+	}
+	else
+	{
+		pthread_mutex_unlock(&phil->mut_frk);
+//		printf("unlock num: %d\n", phil->number);
+		pthread_mutex_unlock(&(phil - 1)->mut_frk);
+//		printf("unlock num: %d\n", (phil - 1)->number);
+	}
+	printf("%llu %d is sleeping\n", get_time(phil->args), phil->number);
+	usleep(phil->args->time_sleep * 1000);
+}
+
+void	try_take_fork(pthread_mutex_t *mut_frk, int num, t_args *args)
+{
+//	printf("befor lock num: %d\n", num);
+	pthread_mutex_lock(mut_frk);
+//	printf("lock num: %d\n", num);
+	printf("%llu %d has taken a fork\n", get_time(args), num);
+}
+
+unsigned long long	get_time(t_args *args)
+{
+	gettimeofday(&args->tv, NULL);
+	return ((args->tv.tv_sec * 1000) + (args->tv.tv_usec / 1000) - args->start_t);
 }
